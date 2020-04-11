21d701da8e41ee91b83fe5e66f482e30c6e309db
Gerasimov Ruslan <gerassimov.ruslan@gmail.coom>
2020-04-11 12:17:07 +0300

    first version of second task (no check if promise rejected)

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -15,37 +15,82 @@
  * Ответ будет приходить в поле {result}
  */
 import Api from '../tools/api';
+import {
+    allPass,
+    ifElse,
+    gt,
+    lt,
+    partial,
+    partialRight,
+    prop,
+    tap,
+    test,
+    compose,
+    equals,
+    always,
+    when, applySpec, identity
+} from "ramda";
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
+
+const stringLength = (string) => string.length;
+
+//При первом вызове возврвщаем функцию редюсер. При втором - запускаем функцию с аргументом
+const composeAsync = function() {
+    const funcs = [...arguments].reverse();
+    return (currentData) => {
+        return funcs.reduce(async (res, f) => {
+            if(res instanceof Promise) {
+                const result = await res;
+                return f(result);
+            } else {
+                return f(res)
+            }
+        }, currentData);
+    }
+};
+
 
 const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
+    const numberConverter = api.get('https://api.tech/numbers/base');
+    const fetchAnimal = api.get('https://animals.tech/');
+    const writeLogTapped = tap(writeLog);
 
-    api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-        writeLog(result);
-    });
-
-    wait(2500).then(() => {
-        writeLog('SecondLog')
-
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
-
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
-}
+    ifElse(
+        allPass([
+            compose(lt(2), stringLength),
+            compose(gt(10), stringLength),
+            test(/^(([1-9][0-9]*)|0)(\.\d+)?$/s)
+        ]),
+        composeAsync(
+            handleSuccess,
+            prop('result'),
+            fetchAnimal,
+            applySpec({ id: identity }),
+            writeLogTapped,
+            (a) => a % 3,
+            writeLogTapped,
+            partialRight(Math.pow, [2]),
+            writeLogTapped,
+            stringLength,
+            writeLogTapped,
+            prop('result'),
+            numberConverter,
+            applySpec({
+                from: always(10),
+                to: always(2),
+                number: identity
+            }),
+            writeLogTapped,
+            when(
+                equals(0),
+                always(1)
+            ),
+            Math.round
+        ),
+        partial(handleError, ['ValidationError'])
+    )(value);
+};
 
 export default processSequence;
